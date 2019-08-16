760a44d3bb24714c6ac922859aa5cda1bc011a2d
julio <sandronister@gmail.com>
2019-08-15 21:58:13 -0300

    second level

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Simples from './components/Simples'
 import ParImpar from './components/ParImpar'
+import {Inverter,Megasega} from './components/Multi'
 
 export default class App extends React.Component {
 	render() {
@@ -9,6 +10,8 @@ export default class App extends React.Component {
 			<View style={styles.container}>
         <Simples texto="Flexivel!!!"/>
         <ParImpar numero="34"/>
+        <Inverter texto='React Native'/>
+        <Megasega numeros={6}/>
 			</View>
 		);
 	}
